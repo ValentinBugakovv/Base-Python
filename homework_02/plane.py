77d5e1d374099f7436d5bab3d997ceffca1f6e37
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2023-06-29 14:25:20 +0300

    -- add class Plane and method load_cargo

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,19 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    def __init__(self, cargo, max_cargo):
+        super().__init__(cargo, max_cargo)
+        self.cargo = cargo
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, numeric: int):
+        if self.cargo + numeric < self.max_cargo:
+            self.cargo += numeric
+        else:
+            raise CargoOverload
