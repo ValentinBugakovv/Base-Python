815107011510d66ee1d1e1a249f4dce905c1123a
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2023-06-29 16:14:39 +0300

    -- fixed

--- homework_02/car.py
+++ homework_02/car.py
@@ -7,8 +7,8 @@ from homework_02.engine import Engine
 
 
 class Car(Vehicle):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, weight=200, fuel=30, fuel_consumption=2):
+        super().__init__(weight, fuel, fuel_consumption)
         self.engine = None
 
     def set_engine(self, engine: Engine):
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -7,13 +7,20 @@ from homework_02.exceptions import CargoOverload
 
 
 class Plane(Vehicle):
-    def __init__(self, cargo, max_cargo):
-        super().__init__(self.weight, self.fuel, self.fuel_consumption)
-        self.cargo = cargo
+    def __init__(self, weight=150, fuel=50, fuel_consumption=5, max_cargo=30):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.cargo = 0
         self.max_cargo = max_cargo
+        self.weight = weight
 
     def load_cargo(self, numeric: int):
-        if self.cargo + numeric < self.max_cargo:
+        if self.cargo + numeric <= self.max_cargo:
             self.cargo += numeric
         else:
             raise CargoOverload
+
+    def remove_all_cargo(self):
+        current_cargo = self.cargo
+        self.cargo = 0
+
+        return current_cargo
