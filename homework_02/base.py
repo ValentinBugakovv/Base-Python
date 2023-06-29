def9ab4cc4f5bf2e83b8c72cb4418d7c4b95520e
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2023-06-29 13:09:56 +0300

    -- add metod start without exceptions.LowFuelError

--- homework_02/base.py
+++ homework_02/base.py
@@ -2,12 +2,15 @@ from abc import ABC
 
 
 class Vehicle(ABC):
-    def __init__(self, weight=None, started=False, fuel=None, fuel_consumption=None):
+    def __init__(self, weight=None, fuel=None, fuel_consumption=None):
         self.weight = weight
+        self.started = False
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
 
-    @classmethod
-    def start():
-        pass
-
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise Exception
