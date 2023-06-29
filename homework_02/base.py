7895f178d4b8da22aa92fef78e4a248ec9b2e0bb
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2023-06-29 12:44:46 +0300

    -- add attribution weight, started, fuel, fuel_consumption

--- homework_02/base.py
+++ homework_02/base.py
@@ -2,4 +2,12 @@ from abc import ABC
 
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight=None, started=False, fuel=None, fuel_consumption=None):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    @classmethod
+    def start():
+        pass
+
